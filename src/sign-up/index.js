8d0fe462dfe234f62b4936d6182e9aff55d91bb6
Lili Boxer <ninaliboxer@gmail.com>
2019-05-13 10:55:28 -0700

    wired up signUp user

---
+++ src/sign-up/index.js
@@ -0,0 +1,13 @@
+import makeUser from './make-user.js';
+
+const userSignUp = document.getElementById('user-sign-up');
+
+userSignUp.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(userSignUp);
+    const user = makeUser(formData);
+
+    console.log(user);
+
+});
