dadb8b419a733d7378eaffe69207a6c91c3a1ae3
Lili Boxer <ninaliboxer@gmail.com>
2019-05-13 16:26:04 -0700

    item page populating correctly;

--- src/object/object.js
+++ src/object/object.js
@@ -1,9 +1,16 @@
 import objectData from '../services/object-data.js';
+import api from '../services/api.js';
 
 const title = document.getElementById('item-title');
 const image = document.getElementById('item-image');
 const description = document.getElementById('item-description');
 
-title.textContent = objectData.title;
-image.src = './assets/objects/' + objectData.id + '.png';
-description.textContent = objectData.description;
+
+
+const searchParams = new URLSearchParams(window.location.search);
+const itemId = searchParams.get('id');
+const item = api.getItem(itemId);
+
+title.textContent = item.title;
+description.textContent = item.description;
+image.src = './assets/objects/' + item.id + '.png';
--- src/services/find-by-id.js
+++ src/services/find-by-id.js
@@ -1,4 +1,5 @@
 import objectData from './object-data.js';
+
 function findById(id) {
     for(let i = 0; i < objectData.length; i++) {
         const object = objectData[i];
