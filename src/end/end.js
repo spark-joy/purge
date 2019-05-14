2acf462bd205de0e9cd0298bf7734cff538f694f
Lili Boxer <ninaliboxer@gmail.com>
2019-05-14 13:43:07 -0700

    items displayed back into results page

--- src/end/end.js
+++ src/end/end.js
@@ -1,5 +1,24 @@
 import api from '../services/api.js';
 import loadProfile from '../load-profile.js';
 
-const profile = loadProfile();
+loadProfile();
 const user = api.getUser(); 
+
+const discardedBox = document.getElementById('discarded-box');
+const keptBox = document.getElementById('kept-box');
+
+const keptItems = user.keptArray;
+const discardedItems = user.discardedArray;
+
+
+function displayIcons(array, location) {
+    for(let index = 0; index < array.length; index++) {
+        const keptItem = array[index];
+        const image = document.createElement('img');
+        image.src = './assets/objects/' + keptItem + '.png';
+        location.appendChild(image);
+    }
+}
+
+displayIcons(keptItems, keptBox);
+displayIcons(discardedItems, discardedBox);
