e7a7ba65e1bb0eddf1f395d24c8c5d59883c735b
Lili Boxer <ninaliboxer@gmail.com>
2019-05-14 09:08:04 -0700

    populated radio buttons back into object.js

--- src/object/object.js
+++ src/object/object.js
@@ -1,9 +1,11 @@
 import api from '../services/api.js';
+import createYes from '../create-yes.js';
+import createNo from '../create-no.js';
 
 const title = document.getElementById('item-title');
 const image = document.getElementById('item-image');
 const description = document.getElementById('item-description');
-// const choiceParentNode = document.getElementById('choice-parent');
+const choiceParentNode = document.getElementById('choice-parent');
 
 
 const searchParams = new URLSearchParams(window.location.search);
@@ -13,6 +15,12 @@ const item = api.getItem(itemId);
 title.textContent = item.title;
 description.textContent = item.description;
 image.src = './assets/objects/' + item.id + '.png';
+
+const yes = createYes();
+const no = createNo();
+
+choiceParentNode.appendChild(no);
+choiceParentNode.appendChild(yes);
 
 
 
