27bb44f499bd284f6c8887705d11211227b1e1e2
Lili Boxer <ninaliboxer@gmail.com>
2019-05-14 11:21:41 -0700

    created completed icon logic in nabigation js

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,6 +1,7 @@
 import api from '../services/api.js';
 import createLink from './create-link.js';
 import objectData from '../services/object-data.js';
+import createVisitedIcon from '../object/create-visited-icon.js';
 
 
 const avatarNode = document.getElementById('avatar');
@@ -10,7 +11,7 @@ const discardedNode = document.getElementById('discarded');
 const nav = document.getElementById('items');
 
 const user = api.getUser();
-
+console.log(user);
 avatarNode.src = './assets/avatars/' + user.character + '.png';
 nameNode.textContent = user.name;
 keptNode.textContent = 'Kept: ' + user.keptArray.length;
@@ -20,7 +21,15 @@ const objects = objectData;
 
 for(let i = 0; i < objects.length; i++) {
     const object = objects[i];
-    const link = createLink(object);
-    nav.appendChild(link);
+    
+    if(user.keptArray.includes(object.id) || user.discardedArray.includes(object.id)) {
+        const visitedIcon = createVisitedIcon(object);
+        console.log(visitedIcon);
+        nav.appendChild(visitedIcon);
+    } else {
+        const link = createLink(object);
+        nav.appendChild(link);
+    
+    }
 }
 
