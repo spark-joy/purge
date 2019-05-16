0a2ba58013d363ad1fbc0b287f4d1316345adb99
Lili Boxer <ninaliboxer@gmail.com>
2019-05-16 09:27:56 -0700

    object data lots of data

--- src/end/kept-array-messages.js
+++ src/end/kept-array-messages.js
@@ -1,5 +1,5 @@
 const keptMessages = {
-    none: `way to go! you kept none of your things!`,
+    none: `way to go! You kept none of your things!`,
     some: `you kept some and discarded some. Not bad! `,
     all: `you kept everything. Really?`
 };
